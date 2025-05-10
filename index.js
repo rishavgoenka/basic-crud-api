20e1136ddf50cc6c3c8d12490109ad5ac7f3f018
Rishav Goenka <goenkarishav09@gmail.com>
2025-05-10 10:22:13 +0530

    update

--- index.js
+++ index.js
@@ -11,8 +11,21 @@ app.use(express.json());
 app.use('/travellers', travellersRoute);
 
 app.get('/', (req, res) => {
-    res.send('Travellers API is running');
+    res.send(`
+        <h2>basic crud api endpoints</h2>
+        GET - <a href="/travellers">localhost:3000/travellers</a> → fetch all users<br />
+        POST - localhost:3000/travellers → create user<br />
+        <pre>
+{
+    "name": "",
+    "age": ,
+    "email": ""
+}</pre><br />
+        PUT - localhost:3000/travellers/:id → update user details<br />
+        DELETE - localhost:3000/travellers/:id → delete user<br />
+    `);
 });
+
 
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
